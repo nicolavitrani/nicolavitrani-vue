cc7bea9690913501f959cc63a94755b56fa49528
Nicola Vitrani <nikussvit@gmail.com>
2023-11-23 22:32:03 +0100

    removed home button animations

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -13,8 +13,8 @@
             <div class="col text-center">
               <h1 class="fw-bold">🚀 Web Developer</h1>
               <p class="mt-3">Dedico la mia competenza e creatività a trasformare idee in realtà digitale, <br>offrendo soluzioni web su misura</p>
-              <router-link to="/contact"><button class="btn btn-lg btn-primary mt-4 me-2 text-uppercase text-white fw-bold magic-hover" data-aos="fade-right">Contatti</button></router-link>
-              <router-link to="/work"><button class="btn btn-lg btn-secondary mt-4 text-uppercase text-white fw-bold magic-hover" data-aos="fade-left">Il mio lavoro</button></router-link>
+              <router-link to="/contact"><button class="btn btn-lg btn-primary mt-4 me-2 text-uppercase text-white fw-bold magic-hover">Contatti</button></router-link>
+              <router-link to="/work"><button class="btn btn-lg btn-secondary mt-4 text-uppercase text-white fw-bold magic-hover">Il mio lavoro</button></router-link>
             </div>
           </div>
         </div>
