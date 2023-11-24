9485a42680d0d64d7ce2275c6a6a6ab220af77f7
Nicola Vitrani <nikussvit@live.it>
2023-11-24 14:11:47 +0100

    added loading on messages data requests

--- src/views/AdminView.vue
+++ src/views/AdminView.vue
@@ -27,7 +27,7 @@
         <h1 class="text-center fw-bold">Messaggi ricevuti</h1>
       </div>
     </div>
-    <div v-show="messages.length > 0" v-for="message in messages" :key="message.id" class="row justify-content-center">
+    <div v-show="messages.length > 0 && !loading" v-for="message in messages" :key="message.id" class="row justify-content-center">
       <div class="col-11 col-md-6 col-lg-4 border-top border-bottom border-gray p-2">
         <div class="d-flex justify-content-between">
           <strong>{{ message.name }}<br><small class="text-gray">{{ message.email }}</small></strong><small>{{ message.date }}</small>
@@ -36,6 +36,12 @@
           <span>{{ message.message }}</span>
           <span @click="deleteMessage(message.id)" class="fs-5" style="cursor: pointer"><font-awesome-icon :icon="['far', 'trash-can']" /></span>
         </div>
+      </div>
+    </div>
+    <div v-show="loading" class="row justify-content-center">
+      <div class="col-12 text-center my-5">
+        <span class="me-2">Caricamento messaggi in corso</span>
+        <font-awesome-icon :icon="['fas', 'arrows-spin']" spin />
       </div>
     </div>
     <div v-show="messages.length === 0" class="row justify-content-center">
@@ -62,7 +68,8 @@ export default {
       username: "",
       password: "",
       isLogged: false,
-      messages: []
+      messages: [],
+      loading: false
     }
   },
   methods: {
@@ -85,6 +92,7 @@ export default {
       this.isLogged = false;
     },
     getAllMessages: function () {
+      this.loading = true;
       axios.get('https://nicolavitrani-a4783-default-rtdb.europe-west1.firebasedatabase.app/messages.json').then(response => {
         console.log("response messages", response);
 
@@ -106,8 +114,11 @@ export default {
 
           // Assegna l'array ordinato a this.messages
           this.messages = messagesArray;
-          console.log("messaggi rifattorizzati", this.messages);
+          this.loading = false;
         }
+      }).catch(err => {
+        console.log("Errore nella richiesta", err);
+        this.loading = false;
       })
     },
     checkIfLogged: function () {
@@ -121,7 +132,9 @@ export default {
     deleteMessage: function (messageId) {
       if (confirm("Eliminare il messaggio?")) {
         axios.delete(`https://nicolavitrani-a4783-default-rtdb.europe-west1.firebasedatabase.app/messages/${messageId}.json`).then(() => {
-          location.reload()
+          this.loading = true;
+          this.getAllMessages();
+          this.loading = false;
         }).catch(() => {
           alert("Impossibile eliminare il messaggio.")
         })
